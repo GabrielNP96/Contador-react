9fe844ecef484f245edb167bbe66f947670dfef2
GabrielNP96 <gabrielnp1996@gmail.com>
2025-03-09 00:13:08 -0300

    Usando TailwindCss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Counter from "./components/Counter";
 function App() {
     return (
         <div>
-            <h1>Meu primeiro projeto React</h1>
             <Counter />
         </div>
     )
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
 
-function Counter() {
-    const [counter, setCounter] = useState<number>(0); //estado do contador
-    return (
-        <div>
-            <h1>Contador : {counter}</h1>
-            <button onClick={() => setCounter (counter + 1)}>Incrementar</button>
-            <button onClick={() => setCounter (counter - 1)}>Decrementar</button>
+const Counter = () => {
+  const [counter, setCounter] = useState<number>(0);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-800">
+      <div className="bg-gray-900 p-6 rounded-xl shadow-xl w-full max-w-md">
+        <h1 className="text-3xl font-semibold text-white mb-6 text-center">
+          Contador: {counter}
+        </h1>
+        <div className="flex justify-around">
+          <button
+            onClick={() => setCounter(counter + 1)}
+            className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition-all duration-300"
+          >
+            Incrementar
+          </button>
+          <button
+            onClick={() => setCounter(counter - 1)}
+            className="bg-red-500 text-white px-6 py-3 rounded-lg hover:bg-red-600 transition-all duration-300"
+          >
+            Decrementar
+          </button>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Counter;
